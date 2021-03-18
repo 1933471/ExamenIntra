c291342e3d4b88b70e481aa280629f8659b21ea4
1933471 <73601302+1933471@users.noreply.github.com>
2021-03-18 12:12:31 -0400

    Add files via upload

---
+++ ExamenIntra/ExamenIntraContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExamenIntra.partieLinq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamenIntra
+{
+
+    public class ExamenIntraContext : DbContext
+    {
+        public DbSet<Fruit> Fruits { get; set; }
+        public DbSet<Personne> Personnes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ExamenIntra;Trusted_Connection = True;");
+    }
+}
---
+++ ExamenIntra/Migrations/20210318151559_AjoutBaseDeDonnees.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamenIntra.Migrations
+{
+    public partial class AjoutBaseDeDonnees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personnes",
+                columns: table => new
+                {
+                    PersonneId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personnes", x => x.PersonneId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fruits",
+                columns: table => new
+                {
+                    FruitId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonneId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fruits", x => x.FruitId);
+                    table.ForeignKey(
+                        name: "FK_Fruits_Personnes_PersonneId",
+                        column: x => x.PersonneId,
+                        principalTable: "Personnes",
+                        principalColumn: "PersonneId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fruits_PersonneId",
+                table: "Fruits",
+                column: "PersonneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fruits");
+
+            migrationBuilder.DropTable(
+                name: "Personnes");
+        }
+    }
+}
